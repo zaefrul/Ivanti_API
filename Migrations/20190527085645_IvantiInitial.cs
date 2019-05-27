961f057a14c59dd48b16cc985fd727ef2b4a58f0
zaefrul <zaefrul@gmail.com>
2019-05-27 16:58:15 +0800

    updatedb

---
+++ Migrations/20190527085645_IvantiInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSM_SOAP_REST_SVC.Migrations
+{
+    public partial class IvantiInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
